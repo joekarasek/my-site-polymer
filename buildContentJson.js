20b76e61b36d1b2c2025d7b530e5298e06003067
Joe Karasek <joseph.karasek@gmail.com>
2016-07-21 11:25:19 -0700

    add chronological sorting to project json build script

--- buildContentJson.js
+++ buildContentJson.js
@@ -5,22 +5,21 @@ var marked = require('marked');
 var fs = require('fs');
 var moment = require('moment');
 
-var data = {
-    projects: []
-};
 
 // Takes one or two unix timestamps and returns a stand date as a string
 var dateHelper = function(startDate, endDate) {
-    // console.log('startDate is ', startDate);
-    // console.log('formated date is ', moment(startDate).format("MMM Do YY"));
     var dateString = "";
     dateString += startDate ? moment(startDate).format("MMM Do 'YY") : "";
     dateString += endDate ? " - " + moment(endDate).format("MMM Do 'YY") : "";
-    console.log(dateString);
     return dateString;
 };
 
+// Glob markdown files, convert yaml front-matter to json, sort, and write
 glob('/Users/karasek/Dev/projects/polymer/app/resources/projects/**/*.md', function(er, files) {
+    var data = {
+        projects: []
+    };
+
     files.forEach(function(file) {
         // read and store content of file
         var contents = fs.readFileSync(file, 'utf8');
@@ -32,7 +31,12 @@ glob('/Users/karasek/Dev/projects/polymer/app/resources/projects/**/*.md', funct
         fileData.attributes.dates = dateHelper(fileData.attributes.startDate, fileData.attributes.endDate);
         data.projects.push(fileData);
     });
-    // console.log('all done', data);
+
+    // Sort projects in json by start date
+    data.projects.sort(function(a,b) {
+       return moment(b.attributes.startDate) - moment(a.attributes.startDate);
+    });
+
     fs.writeFileSync('./app/resources/content.json', JSON.stringify(data, null, '  '));
 });
 
